65d7c0ca2ca375a211d5a70a4d10150fa261f8c2
glorydark <1083215364@qq.com>
2024-02-20 19:03:37 +0800

    增加无玩家在线的判断

--- src/main/java/glorydark/dcurrency/commands/admins/CurrencyGiveCommand.java
+++ src/main/java/glorydark/dcurrency/commands/admins/CurrencyGiveCommand.java
@@ -29,12 +29,16 @@ public class CurrencyGiveCommand extends SubCommand {
             return false;
         }
         if (!CurrencyMain.getRegisteredCurrencies().contains(strings[2])) {
-            sender.sendMessage(CurrencyMain.getLang("message_op_giveCurrency_unregistered_currencies"));
+            sender.sendMessage(CurrencyMain.getLang("message_op_giveCurrency_unregistered_currencies", strings[2]));
             return false;
         }
         switch (strings[1]) {
             case "@a":
                 Collection<Player> players = Server.getInstance().getOnlinePlayers().values();
+                if (players.size() == 0) {
+                    sender.sendMessage(CurrencyMain.getLang("message_op_giveCurrency_no_online_player"));
+                    return true;
+                }
                 for (Player player : players) {
                     CurrencyMain.getProvider().addCurrencyBalance(player.getName(), strings[2], Double.parseDouble(strings[3]));
                 }
@@ -42,6 +46,10 @@ public class CurrencyGiveCommand extends SubCommand {
                 break;
             case "@r":
                 players = Server.getInstance().getOnlinePlayers().values();
+                if (players.size() == 0) {
+                    sender.sendMessage(CurrencyMain.getLang("message_op_giveCurrency_no_online_player"));
+                    return true;
+                }
                 ThreadLocalRandom random = ThreadLocalRandom.current();
                 CurrencyMain.getProvider().addCurrencyBalance(players.toArray(new Player[0])[random.nextInt(0, players.size())].getName(), strings[2], Double.parseDouble(strings[3]));
                 sender.sendMessage(CurrencyMain.getLang("message_op_giveCurrency", strings[1], strings[2], strings[3]));
